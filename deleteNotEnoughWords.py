b0ea3659d44cff7e8a35a79dd6174a69f4a47b21
weiyuanchuang <luobo157@126.com>
2019-05-08 17:28:17 +0800

    Update deleteNotEnoughWords.py

--- deleteNotEnoughWords.py
+++ deleteNotEnoughWords.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # 预处理，首先检测字数，少于300字的，把文件删除掉
+# 目前只能处理当前文件夹
 
 import os
 
@@ -7,7 +8,7 @@ import os
 fns = (fn for fn in os.listdir() if fn.endswith('.txt'))
 
 for fn in fns:
-    if os.path.getsize(fn) < 300:
+    if os.path.getsize(fn) < 700:   #小于700字节的文件，字节删除掉
         os.remove(fn)
     else:
         pass
