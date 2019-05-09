08af3619961b5355529a31287f0a33a6ca79fed5
weiyuanchuang <luobo157@126.com>
2019-05-09 15:00:53 +0800

    Update deleteSpacebarAndN.py

--- deleteSpacebarAndN.py
+++ deleteSpacebarAndN.py
@@ -1,34 +1,8 @@
-#coding:utf8
-#注意：由于未限制文件类型，此程序可以替换path值文件夹下所有文件，包括py文件里面的内容
+# -*- coding: utf-8 -*-
+#暂时只能执行当前文件夹下的txt文件
 #此程序只是替换掉了多余的换行，删除了多余的空格
 
 import os
-
-
-def reset():
-    i = 0
-    path = r".\article\\" #当前文件夹下article文件夹里的内容
-    filelist = os.listdir(path)  # 该文件夹下所有的文件（包括文件夹）
-    for files in filelist:  # 遍历所有文件
-        i = i + 1
-        Olddir = os.path.join(path, files)  # 原来的文件路径
-        if os.path.isdir(Olddir):  # 如果是文件夹则跳过
-            continue
-        filename = os.path.splitext(files)[0]  # 文件名
-        filetype = os.path.splitext(files)[1]  # 文件扩展名
-        filePath = path + filename + filetype
-        # 此处写入需要替换的代码，空格" ",两个空格"  ",空字符"",换行符号"\n",全角空格"\u3000"多被用来执行首行缩进，tab制表符"\t"
-        # 1.把所有空格替换成空字符
-        # 2.把所有换行符替换成空格
-        # 3.把两个空格替换成一个空格，多执行几次，就只剩下一个空格了
-        # 4.然后把空格替换成换行符号
-        alter(filePath, " ", "")
-        alter(filePath, "\u3000", "")
-        alter(filePath, "\n", " ")
-        alter(filePath, "  ", " ")
-        alter(filePath, "  ", " ")
-        alter(filePath, "  ", " ")
-        alter(filePath, " ", "\n")
 
 def alter(file, old_str, new_str):
     """
@@ -45,5 +19,24 @@ def alter(file, old_str, new_str):
             f2.write(line)
     os.remove(file)
     os.rename("%s.bak" % file, file)
+
+fns = (fn for fn in os.listdir() if fn.endswith('.txt'))
+
+for fn in fns:
+    try:
+        # 此处写入需要替换的代码，空格" ",两个空格"  ",空字符"",换行符号"\n",全角空格"\u3000"多被用来执行首行缩进，tab制表符"\t"
+        # 1.把所有空格替换成空字符
+        # 2.把所有全角空格替换成空字符
+        # 3.把所有换行符替换成空格
+        # 4.把两个空格替换成一个空格，多执行几次，就只剩下一个空格了
+        # 5.然后把空格替换成换行符号
+        alter(fn, " ", "")
+        alter(fn, "\u3000", "")
+        alter(fn, "\n", " ")
+        alter(fn, "  ", " ")
+        alter(fn, "  ", " ")
+        alter(fn, "  ", " ")
+        alter(fn, " ", "\n")
+    except:
+        pass
     
-reset()
